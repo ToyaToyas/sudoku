0b8dbee874ee683f265350d0b0807fa191f40aca
ToyaToyas <alihusain.ridwan@gmail.com>
2024-12-05 15:49:59 +0700

    Initial commit

---
+++ src/sudoku/Main.java
@@ -0,0 +1,11 @@
+package sudoku;
+
+public class Main {
+    /** The entry main() entry method */
+    public static void main(String[] args) {
+        // [TODO 1] Check "Swing program template" on how to run
+        //  the constructor of "SudokuMain"
+        //
+        Sudoku game = new Sudoku();
+    }
+}
